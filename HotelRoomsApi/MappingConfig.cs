2a17f0a099352192b6ea2532d8eae096585a24d2
DV <vdletter@gmail.com>
2023-10-13 12:37:32 -0700

    Update config files

---
+++ HotelRoomsApi/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HotelRoomsApi.Models;
+using HotelRoomsApi.Models.Dto;
+
+namespace MagicVilla_VillaAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig() 
+        {
+            CreateMap<Room, RoomDTO>();
+            CreateMap<RoomDTO, Room>();
+
+            CreateMap<Room, RoomCreateDTO>().ReverseMap();
+            CreateMap<Room, RoomUpdateDTO>().ReverseMap();
+
+            CreateMap<RoomNumber, RoomNumberDTO>().ReverseMap();
+            CreateMap<RoomNumber, RoomNumberCreateDTO>().ReverseMap();
+            CreateMap<RoomNumber, RoomNumberUpdateDTO>().ReverseMap();
+            CreateMap<ApplicationUser, UserDTO>().ReverseMap();
+        }
+    }
+}
--- HotelRoomsApi/Program.cs
+++ HotelRoomsApi/Program.cs
@@ -1,11 +1,85 @@
+using HotelRoomsApi.Data;
+using HotelRoomsApi.Models;
+using HotelRoomsApi.Repository;
+using HotelRoomsApi.Repository.IRepository;
+using MagicVilla_VillaAPI;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(option => {
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddScoped<IRoomRepository, RoomRepository>();
+builder.Services.AddScoped<IRoomNumberRepository, RoomNumberRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddAutoMapper(typeof(MappingConfig));
+
+var key = builder.Configuration.GetValue<string>("ApiSettings:JwtSecret");
+builder.Services.AddAuthentication(item =>
+{
+    item.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    item.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(item => {
+    item.RequireHttpsMetadata = false;
+    item.SaveToken = true;
+    item.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+   });
+
+builder.Services.AddControllers(option => {
+    // option.ReturnHttpNotAcceptable=true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 1;
+});
 
 var app = builder.Build();
 
@@ -18,6 +92,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
